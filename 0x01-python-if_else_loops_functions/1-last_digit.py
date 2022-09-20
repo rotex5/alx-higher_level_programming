c9eea93fbec566f15dd1c1ba94fa7e8db207abed
rotex <dantejake48@gmail.com>
2022-09-20 12:25:41 +0100

    del extra whitespace fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ else:
 
 if l_num > 5:
     print(f'Last digit of {number:d} is {l_num:d} \
-            and is greater than 5')
+and is greater than 5')
 elif l_num == 0:
     print(f'Last digit of {number:d} is {l_num:d} and is 0')
 elif l_num < 6:
     print(f'Last digit of {number:d} is {l_num:d} \
-            and is less than 6 and not 0')
+and is less than 6 and not 0')
