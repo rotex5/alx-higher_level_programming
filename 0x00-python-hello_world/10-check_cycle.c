f35bd480575c0d908fee566e265763996df19031
rotex <dantejake48@gmail.com>
2022-09-20 05:29:02 +0100

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for cycle in linked list
+ * @list: the pointer to first node of linked list
+ * Return: 0 if no cycle | 1 if cycle is present
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1 = list;
+	listint_t *temp2 = list;
+
+	if (list == NULL)
+		return (0);
+
+	temp2 = temp2->next;
+
+	while (temp1 && temp2 && temp2->next)
+	{
+		if (temp1 == temp2)
+			return (1);
+
+		temp1 = temp1->next;
+		temp2 = temp2->next->next;
+
+	}
+	return (0);
+}
