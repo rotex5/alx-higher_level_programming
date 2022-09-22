188ff2f42057ede5fea943b93e3df4badc07d977
rotex <dantejake48@gmail.com>
2022-09-22 13:27:50 +0100

    task 0 fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as ad
-a:int = 1
-b:int = 2
+from add_0 import add
 if __name__ == "__main__":
-    print('{:d} + {:d} = {:d}'.format(a, b, ad.add(a, b)))
+    a:int = 1
+    b:int = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
