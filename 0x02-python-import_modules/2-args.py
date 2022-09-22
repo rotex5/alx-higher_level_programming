166a3ef0f0371c78548d9bc3dcfce7175af4bafc
rotex <dantejake48@gmail.com>
2022-09-22 14:25:38 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    length:int = len(argv) - 1
+    if length < 1:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(length))
+        for count, value in enumerate(argv):
+            if count == 0:
+                continue
+            print("{:d}: {}".format(count, value))
