5cd8418a817708a1a31ab4dc80fcec5961eb1a1c
rotex <dantejake48@gmail.com>
2022-09-25 19:37:30 +0100

    code refactor

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    return my_string.translate({ord(i): None for i in 'cC'})
+    # return my_string.translate({ord(i): None for i in 'cC'})
+    length = len(my_string)
+    output = ""
+
+    for i in range(length):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        output += my_string[i]
+
+    return output
