b8d3a88f82499ac25f48701029e3f78fc3898f9e
rotex <dantejake48@gmail.com>
2022-09-25 20:16:41 +0100

    fixx again

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list) - 1
 
-    if not my_list:
-        return None
-
-    for i in range(length, -1, -1):
-        print("{:d}".format(my_list[i]))
+    for i in reversed(my_list):
+        print("{:d}".format(i))
