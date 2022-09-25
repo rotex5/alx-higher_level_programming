0be5329024470008cde2f04120e15a7ed3cd89a7
rotex <dantejake48@gmail.com>
2022-09-25 20:07:07 +0100

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,12 +3,8 @@ def no_c(my_string):
     length = len(my_string)
     output = ""
 
-    if type(my_string) != str:
-        raise Exception("Not a string")
-
     for i in range(length):
         if my_string[i] == "c" or my_string[i] == "C":
             continue
         output += my_string[i]
-
-    return (output)
+    return output
