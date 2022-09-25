b1de82ab23390cbd4eedfa1ef9ffdb7b268e2115
rotex <dantejake48@gmail.com>
2022-09-25 19:50:13 +0100

    added check for not str

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
 def no_c(my_string):
     length = len(my_string)
     output = ""
+
+    if type(my_string) != str:
+        raise Exception("Not a string")
 
     for i in range(length):
         if my_string[i] == "c" or my_string[i] == "C":
             continue
         output += my_string[i]
 
-    return output
+    return (output)
