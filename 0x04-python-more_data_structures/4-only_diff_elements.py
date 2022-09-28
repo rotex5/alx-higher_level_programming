b533df74cb79e502c8ab634fa096db2ba8e318a1
rotex <dantejake48@gmail.com>
2022-09-28 17:07:41 +0100

    task 4 code fix for check 4 and 9

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
+    """
     new_set = set()
-
     for i in set_1:
         for j in set_2:
             if i in set_2 or j in set_1:
@@ -11,3 +11,5 @@ def only_diff_elements(set_1, set_2):
                 new_set.add(i)
                 new_set.add(j)
     return new_set
+    """
+    return set_1 ^ set_2
