aeea24ed9cde9b925190e2ae6fd7ad68b461928b
rotex <dantejake48@gmail.com>
2022-09-28 16:02:37 +0100

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return None
+
+    len_ = len(my_list)
+    new_list = my_list.copy()
+
+    for i in range(len_):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+    return new_list
