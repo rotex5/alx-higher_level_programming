4f93d35b9b97a1fba2fce9b79d0b5a1578f6c2ae
rotex <dantejake48@gmail.com>
2022-09-28 16:58:13 +0100

    task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+
+    for i in set_1:
+        for j in set_2:
+            if i in set_2 or j in set_1:
+                continue
+            else:
+                new_set.add(i)
+                new_set.add(j)
+    return new_set
