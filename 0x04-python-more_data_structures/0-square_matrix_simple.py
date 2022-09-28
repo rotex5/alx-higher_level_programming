c56c27579a90d9b1d37b57b5bbf5960099fb6147
rotex <dantejake48@gmail.com>
2022-09-28 15:37:48 +0100

    task 0 checker fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
     # row = len(matrix)
     column = len(matrix[0])
     temp = [[row[i]**2 for row in matrix] for i in range(column)]
