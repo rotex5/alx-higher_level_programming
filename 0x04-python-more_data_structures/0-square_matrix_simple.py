aba1e644cd00425ba7e9a546616be6c656ff7877
rotex <dantejake48@gmail.com>
2022-09-28 15:36:05 +0100

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    # row = len(matrix)
+    column = len(matrix[0])
+    temp = [[row[i]**2 for row in matrix] for i in range(column)]
+    new_mat = [[row[i] for row in temp] for i in range(column)]
+    return (new_mat)
