6dfc68e9ada9ef0670e54c28e42e9d6b96096166
rotex <dantejake48@gmail.com>
2022-09-28 15:46:26 +0100

    task 0 code refactor

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,17 @@ def square_matrix_simple(matrix=[]):
     if not matrix:
         return None
     # row = len(matrix)
-    column = len(matrix[0])
-    temp = [[row[i]**2 for row in matrix] for i in range(column)]
-    new_mat = [[row[i] for row in temp] for i in range(column)]
-    return (new_mat)
+    # column = len(matrix[0])
+    # temp = [[row[i]**2 for row in matrix] for i in range(column)]
+    # new_mat = [[row[i] for row in temp] for i in range(column)]
+    # return (new_mat)
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    arr = []
+    for i in range(rows):
+        col = []
+        for j in range(cols):
+            col.append(matrix[i][j] ** 2)
+        arr.append(col)
+    return (arr)
