bd0626db0ee45172d9d24a5e6f46974d73df1126
rotex <dantejake48@gmail.com>
2022-10-10 22:14:11 +0100

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,11 +3,10 @@ import sys
 
 
 def safe_function(fct, *args):
-    outcome = None
     try:
-        outcome = fct(args[0], args[1])
+        return fct(args[0], args[1])
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
-        outcome = None
+        return None
     finally:
-        return outcome
+        pass
