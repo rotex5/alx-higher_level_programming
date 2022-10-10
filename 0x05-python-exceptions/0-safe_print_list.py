47affd949df04d19bbb3fb6a48e0f11d415cd12a
rotex <dantejake48@gmail.com>
2022-10-10 15:29:53 +0100

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list:
             print("{}".format(i), end="")
             count += 1
-        print()
     except IndexError:
         pass
     finally:
+        print()
         return count
