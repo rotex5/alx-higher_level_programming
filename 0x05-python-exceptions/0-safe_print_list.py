ce178e29a98549ac366314627c59e3a3451bf16a
rotex <dantejake48@gmail.com>
2022-10-10 15:19:01 +0100

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list:
+            print("{}".format(i), end="")
+            count += 1
+            if count == x:
+                break
+        print()
+        return count
+    except:
+        print("Oops! Something went wrong...")
