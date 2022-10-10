25eedab039a05218af70e2fa5620d4f0242f5b3c
rotex <dantejake48@gmail.com>
2022-10-10 15:32:45 +0100

    code refactor & fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list:
-            print("{}".format(i), end="")
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
             count += 1
+        print()
     except IndexError:
         pass
     finally:
-        print()
         return count
