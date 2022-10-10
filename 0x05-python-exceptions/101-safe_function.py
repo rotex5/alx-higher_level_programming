b7e7cacd60ad634f5c5880327de98a76bcd7af62
rotex <dantejake48@gmail.com>
2022-10-10 22:11:03 +0100

    fixx

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,7 +6,7 @@ def safe_function(fct, *args):
     outcome = None
     try:
         outcome = fct(args[0], args[1])
-    except (ZeroDivisionError, IndexError, TypeError) as err:
+    except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
         outcome = None
     finally:
