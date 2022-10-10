3e6a6584ee1b462bd706aaa7b515ca385b6e7b3e
rotex <dantejake48@gmail.com>
2022-10-10 22:20:52 +0100

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,10 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    outcome = False
+    outcome = True
     try:
         print("{:d}".format(value))
-        outcome = True
     except (TypeError, ValueError) as err:
         outcome = False
         print("Exception: {}".format(err), file=sys.stderr)
