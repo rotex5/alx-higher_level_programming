544161585c6e36766308112b3af666ee979a5c07
rotex <dantejake48@gmail.com>
2022-10-10 15:48:59 +0100

    task 1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
+    outcome = False
     try:
         print("{:d}".format(value))
-        return True
+        outcome = True
     except ValueError:
-        return False
+        outcome = False
+    finally:
+        return outcome
