e641263fb68c32ceca19e945ccf3a9b2846caf17
rotex <dantejake48@gmail.com>
2022-10-11 20:10:36 +0100

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A square module
+"""
+
+
+class Square:
+    """A square class
+    Args:
+        size: size of square
+    """
+
+    def __init__(self, size=0):
+        """ Instantate size"""
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ returns area """
+        return pow(self.__size, 2)
+
+    @property
+    def size(self):
+        """ returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """re-set size"""
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        k = self.__size
+        for i in range(0, k):
+            print(k * "#")
