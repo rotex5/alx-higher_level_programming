ba57a167f9a5ca02038729fa7e48d745daf13d99
rotex <dantejake48@gmail.com>
2022-10-11 19:58:18 +0100

    ValueError fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
         if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
         if size < 0:
-            raise TypeError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
