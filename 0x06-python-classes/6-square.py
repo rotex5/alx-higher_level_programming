c06c63e8992da8b490536e19df7e1c9717c04f07
rotex <dantejake48@gmail.com>
2022-10-11 21:44:05 +0100

    task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+A square module
+"""
+
+
+class Square:
+    """A square class
+    Args:
+        size: size of square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Instantate size"""
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """ returns area """
+        return pow(self.__size, 2)
+
+    @property
+    def size(self):
+        """ returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """re-set size"""
+        if isinstance(value, int) is not True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """returns the position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """re-set position"""
+        if ((isinstance(value[0], int) is True) and (value[0] >= 0)) or (
+                (isinstance(value[1], int) is True) and (value[1] >= 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+        else:
+            k = self.__size
+            if self.__position[0] > 0:
+                for i in range(0, k):
+                    print("{}{}".format(self.__position[0] * "_", k * "#"))
+            else:
+                for i in range(0, k):
+                    print("{}".format(k * "#"))
