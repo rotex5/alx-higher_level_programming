7e12cc948f19cab7a4f54bb89c9c28c844e8306c
rotex <dantejake48@gmail.com>
2022-10-11 19:33:41 +0100

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A square module
+"""
+
+
+class Square:
+    """A square class
+    Args:
+        size: size of square
+    """
+
+    def __init__(self, size=0):
+        """ Instantate size"""
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ returns size """
+        return self.__size
