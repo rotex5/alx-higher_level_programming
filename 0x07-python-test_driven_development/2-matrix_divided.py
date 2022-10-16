6ed30ccc72522c0ece17b1705958f8b33f3f594f
rotex <dantejake48@gmail.com>
2022-10-16 20:02:12 +0100

    pycode styling

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,28 +4,31 @@ This module includes a function that
 divides all elements of a matrix
 """
 
+
 def matrix_divided(matrix, div):
     """
     Returns a new matrix
     """
-    l = len(matrix)
-    for i in range(l):
+    length = len(matrix)
+    for i in range(length):
         if matrix[i] == []:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix(list of lists) of integers/floats")
         for j in matrix[i]:
             if type(j) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix\
+                            (list of lists) of integers/floats")
     i = 0
-    while i < (l-1):
+    while i < (length-1):
         if len(matrix[i]) != len(matrix[i+1]):
             raise TypeError("Each row of the matrix must have the same size")
-        i+=1
+        i += 1
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     if type(div) not in [int, float]:
         raise TypeError("div must be a number of integer/floats")
 
-    new_mat = [[round((j/div), 2) for j in matrix[i]] for i in range(l)]
+    new_mat = [[round((j/div), 2) for j in matrix[i]] for i in range(length)]
     return new_mat
-
