a602aac724c1322d2858ff61b93b10abde379616
rotex <dantejake48@gmail.com>
2022-10-17 15:28:15 +0100

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module includes a
+rectangle class
+"""
+
+
+class Rectangle:
+    """ Class contains several methods to
+    perform operations on a rectangle
+    """
+    pass
