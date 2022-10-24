d4b0e8a85b56adb77578c57e253aa6fec81828d2
rotex <dantejake48@gmail.com>
2022-10-25 00:41:26 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Includes `Square` class"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """square class containing some method"""
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
