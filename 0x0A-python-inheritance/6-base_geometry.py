ea3e9c85269e39496be62842233749fc21253aee
rotex <dantejake48@gmail.com>
2022-10-24 23:49:40 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Includes `BaseGeometry` class"""
+
+
+class BaseGeometry:
+    """baseGeometry class containing some method"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
