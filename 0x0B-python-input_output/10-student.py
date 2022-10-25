fecfd90ff22470897f966f6c72e30bd52d42f6b2
rotex <dantejake48@gmail.com>
2022-10-26 00:07:49 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Includes `Student` class """
+
+
+class Student:
+    """ includes several methods"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new_dict = {}
+
+            for item in attrs:
+                if item in self.__dict__:
+                    new_dict[item] = getattr(self, item)
+            return new_dict
+
+        else:
+            return self.__dict__
