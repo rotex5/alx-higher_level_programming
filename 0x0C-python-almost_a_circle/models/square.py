51d5e7b2cc2d6d882c5aa2ac3c450a63b19ee8c6
rotex <dantejake48@gmail.com>
2022-10-28 17:23:43 +0100

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,4 +20,28 @@ class Square(Rectangle):
         of `Square` class
         """
         return ("[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.size))
+            self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """size getter
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Size setter
+        Attribute:
+            Value(int): value to assign
+        Raises:
+            TypeError: Value must be int
+            ValueError: Value must be > 0
+        """
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")"""
+
+        self.width = value
+        self.height = value
