c72d08e80a9f8120c03767cc23d01409b89df229
rotex <dantejake48@gmail.com>
2022-11-30 23:17:06 +0100

    task 7 fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,8 @@
 -- the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
 );
