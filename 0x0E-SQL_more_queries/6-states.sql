34c383ebd2d0e250c7eb65b8335ef3bf6de5d21a
rotex <dantejake48@gmail.com>
2022-11-30 23:15:32 +0100

    task 6 fixx

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,7 @@
 -- the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
 );
