66af852157e14e623009cacf5a6dad905b0823e0
rotex5 <dantejake48@gmail.com>
2022-12-22 15:21:04 +0100

    task 1-included 'BINARY' in query statement to fix checker 7

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                          port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' \
                 ORDER BY id ASC;")
     states = cur.fetchall()
 
