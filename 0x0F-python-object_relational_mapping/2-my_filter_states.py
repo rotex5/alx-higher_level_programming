ec502e11e56a4942200070da092a556f72f3f80d
rotex5 <dantejake48@gmail.com>
2022-12-22 15:16:22 +0100

    fix pycodestyle requirement/passed code in a function

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,24 +6,31 @@ import sys
 import MySQLdb
 
 
-if __name__ == "__main__":
+def filter_state():
     """
     Querying db for data based on the state passed by user
     """
-    db_connection = MySQLdb.connect(user=sys.argv[1],
-                                    passwd=sys.argv[2],
-                                    db=sys.argv[3],
-                                    host='localhost',
-                                    port=3306,
-                                    charset='utf8')
+    try:
+        db_connection = MySQLdb.connect(user=sys.argv[1],
+                                        passwd=sys.argv[2],
+                                        db=sys.argv[3],
+                                        host='localhost',
+                                        port=3306,
+                                        charset='utf8')
+    except Exception:
+        print("Can't connect to database")
+        return 0
 
     cursor = db_connection.cursor()
-    query = "SELECT id, name FROM states WHERE BINARY name='{}' ORDER BY id ASC;"
+    query = "SELECT id, name FROM states WHERE name='{}' ORDER BY id ASC;"
     cursor.execute(query.format(sys.argv[4]))
     states = cursor.fetchall()
-
     for state in states:
         print(state)
 
     cursor.close()
     db_connection.close()
+
+
+if __name__ == "__main__":
+    filter_state()
