353409bf5496c19bd2f742600b73e5012a8c90f7
rotex5 <dantejake48@gmail.com>
2022-12-22 15:10:21 +0100

    refactored query statement

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
                                     charset='utf8')
 
     cursor = db_connection.cursor()
-    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC;"
+    query = "SELECT id, name FROM states WHERE BINARY name='{}' ORDER BY id ASC;"
     cursor.execute(query.format(sys.argv[4]))
     states = cursor.fetchall()
 
