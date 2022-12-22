9222cd8d6675219ab3bed8e2e7fff157c0241dbf
rotex5 <dantejake48@gmail.com>
2022-12-22 14:50:54 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A script that Lists all states with a name starting
+with N from the a db hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+def filter_state():
+    """
+    Querying db for states that start with 'N'
+    """
+    name_searched = sys.argv[4]
+    try:
+        db_connection = MySQLdb.connect(user=sys.argv[1],
+                                        passwd=sys.argv[2],
+                                        db=sys.argv[3],
+                                        host='localhost',
+                                        port=3306)
+    except Exception:
+        print("Can't connect to database")
+        return 0
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=\
+            '" + name_searched + "' ORDER BY id ASC;")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db_connection.close()
+
+
+if __name__ == "__main__":
+    filter_state()
