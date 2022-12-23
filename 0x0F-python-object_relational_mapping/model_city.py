1f70998bacb97985e1a649fa8338566c358005eb
rotex5 <dantejake48@gmail.com>
2022-12-23 06:57:02 +0100

    model_city pycodesyle fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -11,7 +11,7 @@ class City(Base):
     """
     Class representation of a city
     """
-    __tablename__='cities'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
