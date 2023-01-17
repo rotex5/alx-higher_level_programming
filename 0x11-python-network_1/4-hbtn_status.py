921a9789f4e3bb8eace4327943212189285c5fd6
rotex <dantejake48@gmail.com>
2023-01-17 20:00:33 +0100

    fetches https://alx-intranet.hbtn.io/status using requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    html = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
