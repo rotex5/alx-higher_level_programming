03216bbf2fe345d4f96bff50d333cd77b5c841aa
rotex <dantejake48@gmail.com>
2023-01-18 06:37:07 +0100

    task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) == 3:
+        url = "https://api.github.com/repos/{}/{}/commits".\
+            format(argv[2], argv[1])
+
+        reps = requests.get(url).json()
+        try:
+            for i in range(10):
+                print("{}: {}".format(
+                    reps[i].get("sha"),
+                    reps[i].get("commit").get("author").get("name")))
+        except IndexError:
+            pass
+    else:
+        pass
