2bdabd9d39aef83738276095570b076588fcfcbc
rotex <dantejake48@gmail.com>
2023-01-18 06:19:52 +0100

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# search api
+import requests
+from sys import argv
+
+
+def proc_r(d):
+    resp = requests.get("http://0.0.0.0:5000/search_user", data=d)
+    try:
+        r = resp.json()
+        if r:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    payload = {}
+    if len(argv) > 1:
+        payload["q"] = argv[1]
+    else:
+        payload["q"] = ""
+    proc_r(payload)
