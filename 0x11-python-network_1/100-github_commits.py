cabd7291d1e27a35e204861f5277e4a2af6474a7
rotex <dantejake48@gmail.com>
2023-01-18 09:36:58 +0100

    re-implemented using function

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,18 +5,23 @@ import requests
 from sys import argv
 
 
+def get_latest_commits(repo_name, owner_name):
+
+    url = "https://api.github.com/repos/{}/{}/commits".\
+            format(owner_name, repo_name)
+
+    reps = requests.get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                reps[i].get("sha"),
+                reps[i].get("commit").get("author").get("name")))
+    except Exception:
+        pass
+
+
 if __name__ == "__main__":
     if len(argv) == 3:
-        url = "https://api.github.com/repos/{}/{}/commits".\
-            format(argv[2], argv[1])
-
-        reps = requests.get(url).json()
-        try:
-            for i in range(10):
-                print("{}: {}".format(
-                    reps[i].get("sha"),
-                    reps[i].get("commit").get("author").get("name")))
-        except Exception:
-            pass
-    else:
-        pass
+        repo_name = argv[1]
+        owner_name = argv[2]
+        get_latest_commits(repo_name, owner_name)
