0a85d9fba9a7b0668686112d5dc2048b5bdf84a9
rotex <dantejake48@gmail.com>
2023-01-18 09:01:19 +0100

    removed function

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,7 +3,7 @@
 import requests
 from sys import argv
 
-
+"""
 def proc_r(d):
     resp = requests.post("http://0.0.0.0:5000/search_user", data=d)
     try:
@@ -14,7 +14,7 @@ def proc_r(d):
             print("No result")
     except Exception:
         print("Not a valid JSON")
-
+"""
 
 if __name__ == "__main__":
     payload = {}
@@ -22,4 +22,13 @@ if __name__ == "__main__":
         payload["q"] = argv[1]
     else:
         payload["q"] = ""
-    proc_r(payload)
+    # proc_r(payload)
+    resp = requests.get("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        r = resp.json()
+        if r:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
