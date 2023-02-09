2df4c569b70bf3962d82f3ab2a894b45d2e6dae9
rotex <dantejake48@gmail.com>
2023-02-10 00:20:54 +0100

    print users with completed tasks and number of tasks completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+request.get(url, function (error, _, body) {
+  if (error) {
+    console.log(error);
+  }
+  const json = JSON.parse(body);
+
+  const users = {};
+
+  for (const i of json) {
+    if (i.completed) {
+      if (users[i.userId] == null) {
+        users[i.userId] = 0;
+      }
+      users[i.userId]++;
+    }
+  }
+  console.log(users);
+});
